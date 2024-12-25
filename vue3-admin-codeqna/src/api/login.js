33a3d4f59fe027993968d7be29021fd0ddd03737
cjhqza <jiehuangchen@outlook.com>
2024-12-25 22:53:38 +0800

    更新登录页面以及增加图片验证码接口

--- vue3-admin-codeqna/src/api/login.js
+++ vue3-admin-codeqna/src/api/login.js
@@ -11,12 +11,20 @@
  */
 import request from '@/utils/request'
 
-// 登录接口
+// 管理员用户登录接口
 export const Login = data => {
   return request({
     url: '/admin/system/index/login',
     method: 'post',
     data,
+  })
+}
+
+// 获取图片验证码
+export const GetValidateCode = () => {
+  return request({
+    url: '/admin/system/index/generateValidateCode',
+    method: 'get',
   })
 }
 
